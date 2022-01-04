d8037893df89b4706ddc0334975da438d2374b6c
Shakir Ansari <shakir.ansari@live.com>
2022-01-05 00:06:09 +0530

    Create footer.jsx

---
+++ src/components/footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="bg-dark text-center p-5" id="footer">
+      <div className="align-bottom text-primary">
+        <img src="./resources/images/logo/alpha-x-logo.svg" alt="logo" width="48" />
+        <br />
+        AlphaX Global &copy; 2021
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
