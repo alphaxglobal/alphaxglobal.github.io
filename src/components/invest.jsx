5b4045adb6bcb5afafc481d0eff6f8a680de9e21
Shakir Ansari <shakir.ansari@live.com>
2022-01-05 00:06:14 +0530

    Create invest.jsx

---
+++ src/components/invest.jsx
@@ -0,0 +1,28 @@
+const Invest = () => {
+  return (
+    <section className="bg-white" id="invest">
+      <div className="container p-5 bg-white">
+        <div className="row g-3">
+          <div className="col-md-4 col-sm-12 text-center">
+            <i className="bi bi-graph-up-arrow fs-xxl text-primary"></i>
+          </div>
+          <div className="col-md-8 col-sm-12">
+            <h1 className="mb-3">Why Invest in AlphaX?</h1>
+            <div className="text-muted">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis quaerat molestiae a harum voluptas
+                impedit.
+              </p>
+              <p>
+                Exercitationem laborum vero eius minima magni soluta hic maiores! Adipisci maxime architecto cumque
+                laborum voluptate?
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Invest;
