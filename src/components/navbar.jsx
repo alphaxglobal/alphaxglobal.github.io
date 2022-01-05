6b1c095b01aa5b3ecbecf13175874ef50b2dc2d8
Shakir Ansari <shakir.ansari@live.com>
2022-01-05 14:48:56 +0530

    dataList added

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,23 @@
 import "../css/news.css";
 
 const NavBar = () => {
+  const dataList = [
+    {
+      item: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.",
+    },
+    {
+      item: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.",
+    },
+    {
+      item: "Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.",
+    },
+  ];
+
   return (
     <nav className="navbar navbar-light bg-white fixed-top shadow">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-1 col-md-2 col-sm-12 bg-white">
+      <div className="container-fluid">
+        <div className="row gx-3 w-100">
+          <div className="col-1 bg-white">
             <a className="navbar-brand" href="#">
               <img src="./resources/images/logo/alpha-x-logo.svg" alt="" width="24" height="24" />
               Alpha X
@@ -13,21 +25,11 @@ const NavBar = () => {
           </div>
           <div className="col-lg-11 col-md-10 col-sm-12">
             <div className="ticker">
-              <div className="news">
+              <div className="news w-100">
                 <marquee className="news-content">
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                  </p>
-                  <p>
-                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
-                    totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto
-                  </p>
-                  <p>
-                    Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed
-                    quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
-                    Ut enim ad minima veniam
-                  </p>
+                  {dataList.map((dataItem) => (
+                    <p>{dataItem.item}</p>
+                  ))}
                 </marquee>
               </div>
             </div>
